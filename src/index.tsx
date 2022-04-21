aa2cbd6583e303ca84704669580143b92253a363
sweetxoju <sweetxoju@gmail.com>
2022-04-21 15:04:34 +0900

    feat: add ag-grid

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { LicenseManager } from 'ag-grid-enterprise';
+
+LicenseManager.setLicenseKey('your license key');
 
 ReactDOM.render(
   <React.StrictMode>
