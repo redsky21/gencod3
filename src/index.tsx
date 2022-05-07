8783d6f56b5a6a57334fa6f2e9f80f8a8bd81a43
pig1 <redsky21@lgcns.com>
2022-05-07 10:12:24 +0900

    license remove

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { LicenseManager } from 'ag-grid-enterprise';
 
-LicenseManager.setLicenseKey(
-  'CompanyName=Geonwoo Solution Co.Ltd_on_behalf_of_LG Electronics Inc,LicensedGroup=Enterprise Task Support,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=10,LicensedProductionInstancesCount=0,AssetReference=AG-016785,ExpiryDate=5_July_2022_[v2]_MTY1Njk3NTYwMDAwMA==883cedbc56a08758c5a6508e61387f3a',
-);
-
 ReactDOM.render(
   <React.StrictMode>
     <div className="wrap">
